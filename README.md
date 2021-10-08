8c424970f21d9fc3a6c0f8cc57b2d339eb4ab4ee
Eoin McAfee <83226740+eoinmcafee00@users.noreply.github.com>
2021-10-08 10:32:45 +0100

    Snyk Scan (#2)
    * user can provide dockerfile and image name to kick off scan
    * update params in readme
    * change dind image version
    * update logging logic for docker
    * fix issue with publish
    * update image .drone.yml
    * provide ability to set severity level
    * output result to file
    * wrap the scan command in a shell
    * create summary data from snyk
    * updating logic on writing summary
    * update readme to show different auth .drone.yml
    
    Co-authored-by: TP Honey <tp@harness.io>

--- README.md
+++ README.md
@@ -1,25 +1,77 @@
 A plugin to scan container images for vulnerabilities using snyk.
 
+By using this plugin you agree to accept the terms and conditions set out by snyk and docker. More details can be found [here](https://snyk.io/learn/docker-security-scanning/)
+
 # Usage
 
 The following settings changes this plugin's behavior.
 
-* param1 (optional) does something.
-* param2 (optional) does something different.
+* dockerfile uri to dockerfile in repo.
+* image the name of the image you wish to scan.
+* snyk (optional) auth token for snyk (without this you will get 10 scans a month)
+* severity threshold can be set the limit the results return, low, medium, high & critical are acceptable inputs
 
-Below is an example `.drone.yml` that uses this plugin.
+> Either a docker username/passport or Snyk token can be used for authentication
+
+> Notice: Be aware that the Docker plugin currently requires privileged capabilities, otherwise the integrated Docker daemon is not able to start.
+
+Below is an example `.drone.yml` that uses this plugin using snyk auth
 
 ```yaml
 kind: pipeline
 name: default
 
 steps:
-- name: run drone-plugins/drone-snyk plugin
+- name: scan
   image: drone-plugins/drone-snyk
   pull: if-not-exists
+  privileged: true
   settings:
-    param1: foo
-    param2: bar
+      dockerfile: link to dockerfile in repo
+      image: image name
+      snyk:
+        from_secret: snyk
+```
+Below is an example `.drone.yml` that uses this plugin using docker auth
+
+```yaml
+kind: pipeline
+name: default
+
+steps:
+- name: scan
+  image: drone-plugins/drone-snyk
+  pull: if-not-exists
+  privileged: true
+  settings:
+      dockerfile: link to dockerfile in repo
+      image: image name
+      username:
+        from_secret: username
+      password:
+        from_secret: password
+```
+# Additional Settings
+```text
+REGISTRY // docker registry address
+USERNAME // Docker registry username 
+PASSWORD // Docker registry password 
+EMAIL    // Docker registry email
+CONFIG   // Docker Auth Config
+
+REGISTRY            // Docker registry
+MIRROR              // Docker registry mirror
+INSECURE            // Docker daemon enable insecure registries
+STORAGE_DRIVER      // Docker daemon storage driver
+STORAGE_PATH        // Docker daemon storage path
+DAEMON_OFF          // Docker daemon is disabled (already running)
+DEBUG               // Docker daemon started in debug mode
+BIP                 // Docker daemon network bridge IP address
+CUSTOM_DNS          // Docker daemon dns server
+CUSTOM_DNS_SEARCH   // Docker daemon dns search domain
+MTU                 // Docker daemon mtu setting
+IPV6                // Docker daemon IPv6 networking
+EXPERIMENTAL        // Docker daemon enable experimental mode
 ```
 
 # Building
@@ -41,7 +93,7 @@ docker build -t drone-plugins/drone-snyk -f docker/Dockerfile .
 Execute the plugin from your current working directory:
 
 ```text
-docker run --rm -e PLUGIN_PARAM1=foo -e PLUGIN_PARAM2=bar \
+docker run --rm -e PLUGIN_IMAGE=image -e PLUGIN_DOCKERFILE=dockerfile -e PLUGIN_USERNAME=username -e PLUGIN_PASSWORD=password --privileged=true  \
   -e DRONE_COMMIT_SHA=8f51ad7884c5eb69c11d260a31da7a745e6b78e2 \
   -e DRONE_COMMIT_BRANCH=master \
   -e DRONE_BUILD_NUMBER=43 \
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -4,22 +4,366 @@
 
 package plugin
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const dockerExe = "/usr/local/bin/docker"
+const dockerdExe = "/usr/local/bin/dockerd"
+const dockerHome = "/root/.docker/"
+const bashExe = "/bin/sh"
+
+type (
+	// Login defines Docker  parameters.
+	Login struct {
+		Registry string `envconfig:"PLUGIN_REGISTRY" default:"https://index.docker.io/v1/"` // Docker registry address
+		Username string `envconfig:"PLUGIN_USERNAME"`                                       // Docker registry username
+		Password string `envconfig:"PLUGIN_PASSWORD"`                                       // Docker registry password
+		Email    string `envconfig:"PLUGIN_EMAIL"`                                          // Docker registry email
+		Config   string `envconfig:"PLUGIN_CONFIG"`                                         // Docker Auth Config
+	}
+
+	Daemon struct {
+		PluginRegistry string   `envconfig:"PLUGIN_REGISTRY" default:"https://index.docker.io/v1/"` // Docker registry
+		Mirror         string   `envconfig:"PLUGIN_MIRROR"`                                         // Docker registry mirror
+		Insecure       bool     `envconfig:"PLUGIN_INSECURE"`                                       // Docker daemon enable insecure registries
+		StorageDriver  string   `envconfig:"PLUGIN_STORAGE_DRIVER"`                                 // Docker daemon storage driver
+		StoragePath    string   `envconfig:"PLUGIN_STORAGE_PATH" default:"/var/lib/docker"`         // Docker daemon storage path
+		Disabled       bool     `envconfig:"PLUGIN_DAEMON_OFF"`                                     // Docker daemon is disabled (already running)
+		Debug          bool     `envconfig:"PLUGIN_DEBUG"`                                          // Docker daemon started in debug mode
+		Bip            string   `envconfig:"PLUGIN_BIP"`                                            // Docker daemon network bridge IP address
+		DNS            []string `envconfig:"PLUGIN_CUSTOM_DNS"`                                     // Docker daemon dns server
+		DNSSearch      []string `envconfig:"PLUGIN_CUSTOM_DNS_SEARCH"`                              // Docker daemon dns search domain
+		MTU            string   `envconfig:"PLUGIN_MTU"`                                            // Docker daemon mtu setting
+		IPv6           bool     `envconfig:"PLUGIN_IPV6"`                                           // Docker daemon IPv6 networking
+		Experimental   bool     `envconfig:"PLUGIN_EXPERIMENTAL"`                                   // Docker daemon enable experimental mode
+	}
+
+	ScanDocker struct {
+		BaseImage            string `json:"baseImage"`
+		BaseImageRemediation struct {
+			Code   string `json:"code"`
+			Advice []struct {
+				Message string `json:"message"`
+				Bold    bool   `json:"bold,omitempty"`
+			} `json:"advice"`
+		} `json:"baseImageRemediation"`
+	}
+
+	ScanResults struct {
+		Vulnerabilities []struct {
+			PackageName          string `json:"packageName"`
+			Severity             string `json:"severity"`
+			SeverityWithCritical string `json:"severityWithCritical"`
+		} `json:"vulnerabilities"`
+		Ok              bool       `json:"ok"`
+		DependencyCount int        `json:"dependencyCount"`
+		Docker          ScanDocker `json:"docker"`
+		Summary         string     `json:"summary"`
+		UniqueCount     int        `json:"uniqueCount"`
+		ProjectName     string     `json:"projectName"`
+		Platform        string     `json:"platform"`
+		Path            string     `json:"path"`
+	}
+
+	ScanSummary struct {
+		Issues struct {
+			Critical int64 `json:"critical"`
+			High     int64 `json:"high"`
+			Medium   int64 `json:"medium"`
+			Low      int64 `json:"low"`
+		}
+		Docker      ScanDocker `json:"docker"`
+		Summary     string     `json:"summary"`
+		UniqueCount int        `json:"uniqueCount"`
+		ProjectName string     `json:"projectName"`
+		Platform    string     `json:"platform"`
+		Path        string     `json:"path"`
+	}
+)
 
 // Args provides plugin execution arguments.
 type Args struct {
 	Pipeline
-
-	// Level defines the plugin log level.
-	Level string `envconfig:"PLUGIN_LOG_LEVEL"`
-
-	// TODO replace or remove
-	Param1 string `envconfig:"PLUGIN_PARAM1"`
-	Param2 string `envconfig:"PLUGIN_PARAM2"`
+	Login             Login
+	Daemon            Daemon
+	Level             string `envconfig:"PLUGIN_LOG_LEVEL"`
+	Dockerfile        string `envconfig:"PLUGIN_DOCKERFILE"`
+	Image             string `envconfig:"PLUGIN_IMAGE" required:"true"`
+	AuthToken         string `envconfig:"PLUGIN_SNYK"`
+	SeverityThreshold string `envconfig:"PLUGIN_SEVERITY_THRESHOLD"`
 }
 
 // Exec executes the plugin.
 func Exec(ctx context.Context, args Args) error {
-	// write code here
+	if !args.Daemon.Disabled {
+		startDaemon(args.Daemon)
+	}
+
+	fmt.Printf("Current Unix Time: %v\n", time.Now().Unix())
+	var results ScanResults
+	severityLevel := strings.ToLower(args.SeverityThreshold)
+	switch severityLevel {
+	case "critical",
+		"high",
+		"medium",
+		"low":
+		fmt.Printf("Severity Threshold level set at %s\n", severityLevel)
+	case "":
+		fmt.Printf("Severity Threshold level not set.")
+	default:
+		return fmt.Errorf("invalid severity level input, must be critical, high, medium or low")
+	}
+
+	// poll the docker daemon until it is started. This ensures the daemon is
+	// ready to accept connections before we proceed.
+	for i := 0; ; i++ {
+		cmd := commandInfo()
+		err := cmd.Run()
+		if err == nil {
+			break
+		}
+		if i == 15 {
+			fmt.Println("Unable to reach Docker Daemon after 15 attempts.")
+			break
+		}
+		time.Sleep(time.Second * 1)
+	}
+	// docker login
+	// for debugging purposes, log the type of authentication
+	// credentials that have been provided.
+	switch {
+	case args.Login.Password != "" && args.Login.Config != "":
+		fmt.Println("Detected registry credentials and registry credentials file")
+	case args.Login.Password != "":
+		fmt.Println("Detected registry credentials")
+	case args.Login.Config != "":
+		fmt.Println("Detected registry credentials file")
+	default:
+		fmt.Println("Registry credentials or Docker config not provided. Guest mode enabled.")
+	}
+
+	// create Auth Config File
+	if args.Login.Config != "" {
+		os.MkdirAll(dockerHome, 0600)
+
+		path := filepath.Join(dockerHome, "config.json")
+		err := ioutil.WriteFile(path, []byte(args.Login.Config), 0600)
+		if err != nil {
+			return fmt.Errorf("error writing config.json: %s", err)
+		}
+	}
+
+	// login to the Docker registry
+	if args.Login.Password != "" {
+		cmd := commandLogin(args.Login)
+		raw, err := cmd.CombinedOutput()
+		if err != nil {
+			out := string(raw)
+			out = strings.Replace(out, "WARNING! Using --password via the CLI is insecure. Use --password-stdin.", "", -1)
+			fmt.Println(out)
+			return fmt.Errorf("error authenticating: exit status 1")
+		}
+	}
+
+	var cmds []*exec.Cmd
+	if args.AuthToken != "" {
+		cmds = append(cmds, snykLogin(args.AuthToken))
+	} else {
+		fmt.Println("Snyk credentials not provided. Unauthenticated mode only allows 10 scans a month")
+	}
+
+	cmds = append(cmds, scanResultsToFile(args.Image, args.Dockerfile, severityLevel))
+	cmds = append(cmds, scan(args.Image, args.Dockerfile, severityLevel))
+	// execute all commands in batch mode.
+	for _, cmd := range cmds {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		trace(cmd)
+
+		err := cmd.Run()
+
+		if err != nil {
+			// required so it doesn't exit on scan file stage
+			if !isCommandScanFile(cmd.Args) {
+				return err
+			} else {
+				err = MapSummaryResults(results)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 	return nil
+}
+
+func MapSummaryResults(results ScanResults) error {
+	data, err := os.ReadFile("/tmp/output.json")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = json.Unmarshal(data, &results)
+
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	var summary ScanSummary
+	err = json.Unmarshal(data, &summary)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	for _, v := range results.Vulnerabilities {
+		switch v.Severity {
+		case "critical":
+			summary.Issues.Critical = summary.Issues.Critical + 1
+		case "high":
+			summary.Issues.High = summary.Issues.High + 1
+		case "medium":
+			summary.Issues.Medium = summary.Issues.Medium + 1
+		case "low":
+			summary.Issues.Low = summary.Issues.Low + 1
+		}
+	}
+	r, err := json.Marshal(summary)
+	fmt.Println(string(r))
+	return nil
+}
+
+func commandLogin(login Login) *exec.Cmd {
+	if login.Email != "" {
+		return commandLoginEmail(login)
+	}
+	return exec.Command(
+		dockerExe, "login",
+		"-u", login.Username,
+		"-p", login.Password,
+		login.Registry,
+	)
+}
+
+func commandLoginEmail(login Login) *exec.Cmd {
+	return exec.Command(
+		dockerExe, "login",
+		"-u", login.Username,
+		"-p", login.Password,
+		"-e", login.Email,
+		login.Registry,
+	)
+}
+
+// helper function to create the docker info command.
+func commandInfo() *exec.Cmd {
+	return exec.Command(dockerExe, "info")
+}
+
+func scanResultsToFile(image, dockerfile, severityLevel string) *exec.Cmd {
+	args := []string{
+		"docker scan --json --group-issues",
+	}
+	if severityLevel != "" {
+		args = append(args, "--severity="+severityLevel)
+	}
+	args = append(args, image)
+	if dockerfile != "" {
+		args = append(args, "--file="+dockerfile)
+	}
+	args = append(args, "> /tmp/output.json")
+
+	return exec.Command(bashExe, "-c", strings.Join(args, " "))
+}
+
+func scan(image, dockerfile, severityLevel string) *exec.Cmd {
+	args := []string{"scan"}
+	if severityLevel != "" {
+		args = append(args, "--severity="+severityLevel)
+	}
+	args = append(args, image)
+	if dockerfile != "" {
+		args = append(args, "--file="+dockerfile)
+	}
+	args = append(args, "--accept-license")
+	return exec.Command(dockerExe, args...)
+}
+
+func snykLogin(token string) *exec.Cmd {
+	args := []string{"scan", "--login"}
+	args = append(args, "--token", token, "--accept-license")
+	return exec.Command(dockerExe, args...)
+}
+
+// trace writes each command to stdout with the command wrapped in an xml
+// tag so that it can be extracted and displayed in the logs.
+func trace(cmd *exec.Cmd) {
+	fmt.Fprintf(os.Stdout, "+ %s\n", strings.Join(cmd.Args, " "))
+}
+
+func isCommandScanFile(args []string) bool {
+	return args[0] == "/bin/sh"
+}
+
+// helper function to create the docker daemon command.
+func commandDaemon(daemon Daemon) *exec.Cmd {
+	args := []string{
+		"--data-root", daemon.StoragePath,
+		"--host=unix:///var/run/docker.sock",
+	}
+
+	if _, err := os.Stat("/etc/docker/default.json"); err == nil {
+		args = append(args, "--seccomp-profile=/etc/docker/default.json")
+	}
+
+	if daemon.StorageDriver != "" {
+		args = append(args, "-s", daemon.StorageDriver)
+	}
+	if daemon.Insecure && daemon.PluginRegistry != "" {
+		args = append(args, "--insecure-registry", daemon.PluginRegistry)
+	}
+	if daemon.IPv6 {
+		args = append(args, "--ipv6")
+	}
+	if len(daemon.Mirror) != 0 {
+		args = append(args, "--registry-mirror", daemon.Mirror)
+	}
+	if len(daemon.Bip) != 0 {
+		args = append(args, "--bip", daemon.Bip)
+	}
+	for _, dns := range daemon.DNS {
+		args = append(args, "--dns", dns)
+	}
+	for _, dnsSearch := range daemon.DNSSearch {
+		args = append(args, "--dns-search", dnsSearch)
+	}
+	if len(daemon.MTU) != 0 {
+		args = append(args, "--mtu", daemon.MTU)
+	}
+	if daemon.Experimental {
+		args = append(args, "--experimental")
+	}
+	return exec.Command(dockerdExe, args...)
+}
+
+func startDaemon(d Daemon) {
+	cmd := commandDaemon(d)
+	if d.Debug {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	} else {
+		cmd.Stdout = ioutil.Discard
+		cmd.Stderr = ioutil.Discard
+	}
+	go func() {
+		trace(cmd)
+		cmd.Run()
+	}()
 }
